5da414606dc1a4c866f567e2d714ea7a6fc6c89d
fdch <fch226@nyu.edu>
2022-11-02 12:38:20 +0100

    submission

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,11 +1,63 @@
-// console.log('Contentscript injected');
+console.log('Contentscript injected');
 
-// let element = document.createElement('div');
-// const injectedText = document.createTextNode(`Extension-like element`);
+// just use the 'section' and query it later
+const section = document.querySelector(
+    '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section'
+) 
+const parentElement = section.querySelector('h1');
+const questionElement = section.querySelector('div:nth-child(3)');
 
-// element.appendChild(injectedText);
+// get the data from the DOM
+const txt = questionElement.querySelector("p:nth-child(2)").innerHTML;
+const bdg = questionElement.querySelector("p:nth-child(3)").innerHTML;
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
-// parentElement.appendChild(element);
+const injectedText = document.createTextNode(`${txt}: ${bdg}`);
+
+
+// Button element 
+let button = document.createElement('button');
+
+// Button hard-coded styling
+button.style.background = '#323c4e';
+button.style.color = 'white';
+button.style.fontSize = '0.5em';
+button.style.padding = '10px';
+button.style.fontWeight = 'bold';
+button.style.border = 'None';
+button.style.borderRadius = '10px';
+button.style.margin = '3px 0 0 0';
+
+button.appendChild(injectedText);
+
+// Popup element with more information
+let popup = document.createElement('div');
+
+// Popup hard-coded styling
+popup.style.display = "None"; // hidden at first
+popup.style.border = '2px solid #2156c1';
+popup.style.borderRadius = '10px';
+popup.style.background = 'white';
+popup.style.margin = '3px 0 0 3px';
+popup.style.width = '50vmax';
+popup.style.padding = '1em';
+popup.style.fontSize = '0.5em';
+
+// the dummy data to show
+// FIXME: this should be fetched, not here though
+const dummyClimateData = document.createTextNode(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat.`)
+
+popup.appendChild(dummyClimateData);
+
+
+// Button hover functionality
+button.onmouseenter = () => {
+    popup.style.display = "block";
+}
+button.onmouseleave = () => {
+    popup.style.display = "None";
+}
+
+parentElement.appendChild(button);
+parentElement.appendChild(popup);
+
--- src/views/user/UserHistoryView.tsx
+++ src/views/user/UserHistoryView.tsx
@@ -1,5 +1,76 @@
-import { ReactElement } from 'react';
+import { makeStyles } from '@mui/styles';
+import { ReactElement, useEffect, useState } from 'react';
+import { getMonthName } from './UserDashboardView';
+import dummyData from '../../../dummyData.json';
+
+
+const useStyles = makeStyles(() => ({
+  dashboardWrapper: {
+      width: '100%',
+  },
+  table: {
+    width: '100%',
+    background: 'lightgrey',
+    padding: '10px',
+    borderRadius: '10px'
+  },
+  tableHead: {
+    fontWeight: 'bold',
+    fontSize: '1.5em',
+  },
+  tableBody: {
+    width: '100%'
+  }
+}));
 
 export const UserHistoryView = (): ReactElement => {
-  return <div>{'I could be a table'}</div>;
-};
+    const classes = useStyles();
+    const [hist, setHist] = useState<{ 
+        date: string,
+        reward: number,
+        savings: number;
+    }[]>([]);
+  
+    useEffect(() => {
+      async function fetchData() {
+
+        const rewardHistory = dummyData.HISTORY_DATA;
+  
+        const hist = rewardHistory.map((x) => {
+          return {
+            date: `${getMonthName(x.month)} ${x.year}`,
+            reward: x.reward,
+            savings: x.co2_saved
+          };
+        });
+  
+        setHist(hist);
+      }
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div className={classes.dashboardWrapper}>
+        <table className={classes.table}>
+            <thead className={classes.tableHead}>
+            <tr>         
+                <td>Date</td>
+                <td>Reward</td>
+                <td>CO2 Savings</td>
+            </tr>
+            </thead>
+            <tbody className={classes.tableBody}>
+                {hist.map((x, i) => {
+                return <tr key={i+"-trip"}>
+                         <td>{x.date}</td>
+                         <td>{x.reward}</td>
+                         <td>{x.savings}</td>
+                       </tr>;
+              })}
+            </tbody>
+        </table>
+      </div>
+    );
+  };
+  
